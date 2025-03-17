ea3207febf7b9624158cc478607c88d730db050c
ansmycode <3312308240@qq.com>
2025-03-17 21:58:19 +0800

    feat:将MDX安装进项目中并进行配置,编写mdx导致的一些问题readme文件中

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
-
+import remarkGfm from "remark-gfm"; // GitHub Flavored Markdown 插件
+import createMDX from "@next/mdx";
+import rehypeHighlight from "rehype-highlight"; // 代码高亮插件
+import rehypeCodeTitles from "rehype-code-titles";
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+// 使用createMDX并配置插件
+// 部分插件与turbopack冲突 , 使用插件时使用自定义的启动项npm run devnwt
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      [rehypeCodeTitles, {}],
+      [
+        rehypeHighlight,
+        {
+          ignoreMissing: true,
+          languages: {
+            typescript: require("highlight.js/lib/languages/typescript"),
+            bash: require("highlight.js/lib/languages/bash"),
+          },
+        },
+      ],
+    ],
+  },
+});
+export default withMDX(nextConfig);
